e3f3b56edaf372f83a05911de3f59ab983a6b51f
Sumon Ahmed <bdsumon4u@gmail.com>
2021-06-10 12:24:32 +0600

    CallForOrder

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -123,8 +123,8 @@
                                     </div>
                                 </div>
                                 <div class="call-for-order">
-                                    <img src="{{ asset('call-now-icon-20.jpg') }}" width="287" height="68" alt="Call For Order">
-                                    <div style="padding: 10px;margin-bottom: 10px;font-weight: bold;color: red;padding-left: 85px;">
+                                    <img src="{{ asset('call-now-icon-20.jpg') }}" width="155" alt="Call For Order">
+                                    <div style="padding: 10px;margin-bottom: 10px;font-weight: bold;color: red;">
                                         @php($arr = (string)(setting('call_for_order') ?? ''))
                                         {!! implode('<br>', explode(' ', $arr)) !!}
                                     </div>
