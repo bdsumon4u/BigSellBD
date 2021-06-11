f96d875a03810b4cfc20d2d9a52a46e6a579afdc
Sumon Ahmed <bdsumon4u@gmail.com>
2021-06-11 21:01:57 +0600

    Double Invoice

---
+++ resources/views/admin/orders/invoice.blade.php
@@ -0,0 +1,98 @@
+<div class="{{ $class ?? '' }}">
+    <div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="media">
+                    <div class="media-left">
+                        <img class="media-object" src="{{asset($logo->mobile)}}" alt="" width="180" height="54">
+                    </div>
+                    <div class="media-body m-l-20">
+                        <h4 class="media-heading">{{ $company->name }}</h4>
+                        <p class="m-0"><span class="digits">{{ $company->phone }}</span></p>
+                    </div>
+                </div>
+                <!-- End Info-->
+            </div>
+            <div class="col-sm-6">
+                <div class="text-md-right">
+                    <h3>Invoice #<span class="digits counter">{{ $order->id }}</span></h3>
+                    <p>
+                        Ordered At: {{ $order->created_at->format('M') }}<span class="digits"> {{ $order->created_at->format('d, Y') }}</span>
+                        {{--                                            <br> Invoiced At: {{ date('M') }}<span class="digits"> {{ date('d, Y') }}</span>--}}
+                    </p>
+                </div>
+                <!-- End Title-->
+            </div>
+        </div>
+    </div>
+    <hr>
+    <!-- End InvoiceTop-->
+    <div class="row">
+        <div class="col-md-4">
+            <div class="media">
+                <div class="media-body m-l-20">
+                    <h5 class="media-heading">{{ $order->name }}</h5>
+                    <p class="m-0">{{ $order->phone }}</p>
+                    <div>{{ $order->address }}</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="text-md-right" id="project">
+                <h6>Note</h6>
+                <p>{{ $order->note ?? 'N/A' }}</p>
+            </div>
+        </div>
+    </div>
+    <!-- End Invoice Mid-->
+    <div>
+        <div class="table-responsive invoice-table" id="table">
+            <table class="table table-sm table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th width="95">Price</th>
+                    <th width="10">Quantity</th>
+                    <th width="95">Total</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($order->products as $product)
+                    <tr>
+                        <td>
+                            <img src="{{ $product->image }}" alt="Image" width="80" height="70">
+                        </td>
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->price }}</td>
+                        <td>{{ $product->quantity }}</td>
+                        <td>{{ $product->quantity * $product->price }}</td>
+                    </tr>
+                @endforeach
+                <tr>
+                    <th rowspan="5" colspan="3" style="text-align: center; vertical-align: middle; font-size: 24px;">
+                        <span style="font-weight: 400;">Condition</span>: TK. {{ $order->data->shipping_cost + $order->data->subtotal - ($order->data->advanced ?? 0) - ($order->data->discount ?? 0) }}
+                    </th>
+                </tr>
+                <tr>
+                    <th>Subtotal</th>
+                    <th>{{ $order->data->subtotal }}</th>
+                </tr>
+                <tr>
+                    <th>Advanced</th>
+                    <th>{{ $order->data->advanced ?? 0 }}</th>
+                </tr>
+                <tr>
+                    <th>Shipping</th>
+                    <th>{{ $order->data->shipping_cost }}</th>
+                </tr>
+                <tr>
+                    <th>Discount</th>
+                    <th>{{ $order->data->discount ?? 0 }}</th>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <!-- End Table-->
+    </div>
+</div>
--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -7,12 +7,15 @@
 
 @push('styles')
 <style>
+.only-print {
+    display: none;
+}
 @media print {
     html, body {
-    height:100vh;
-    margin: 0 !important;
-    padding: 0 !important;
-    overflow: hidden;
+        height:100vh;
+        margin: 0 !important;
+        padding: 0 !important;
+        overflow: hidden;
     }
 
     .main-nav {
@@ -26,8 +29,17 @@
     }
 
     .page-body {
+        font-size: 20px;
+        margin-top: 0 !important;
         margin-left: 0 !important;
         page-break-after: always;
+    }
+    .page-body p {
+        font-size: 16px !important;
+    }
+    .only-print {
+        display: block;
+        padding-top: 4rem;
     }
 }
 </style>
@@ -51,100 +63,10 @@
             <div class="card-body">
                 <div class="invoice">
                     <div>
-                        <div>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="media">
-                                        <div class="media-left">
-                                            <img class="media-object" src="{{asset($logo->mobile)}}" alt="" width="180" height="54">
-                                        </div>
-                                        <div class="media-body m-l-20">
-                                            <h4 class="media-heading">{{ $company->name }}</h4>
-                                            <p class="m-0">{{ $company->email }}<br><span class="digits">{{ $company->phone }}</span></p>
-                                            <div>{{ $order->address }}</div>
-                                        </div>
-                                    </div>
-                                    <!-- End Info-->
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="text-md-right">
-                                        <h3>Invoice #<span class="digits counter">{{ $order->id }}</span></h3>
-                                        <p>
-                                            Ordered At: {{ $order->created_at->format('M') }}<span class="digits"> {{ $order->created_at->format('d, Y') }}</span>
-                                            <br> Invoiced At: {{ date('M') }}<span class="digits"> {{ date('d, Y') }}</span>
-                                        </p>
-                                    </div>
-                                    <!-- End Title-->
-                                </div>
-                            </div>
-                        </div>
-                        <hr>
-                        <!-- End InvoiceTop-->
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="media">
-                                    <div class="media-body m-l-20">
-                                        <h4 class="media-heading">{{ $order->name }}</h4>
-                                        <p><span class="digits">{{ $order->email }}</span><br>{{ $order->phone }}</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="text-md-right" id="project">
-                                    <h6>Note</h6>
-                                    <p>{{ $order->note ?? 'N/A' }}</p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Invoice Mid-->
-                        <div>
-                            <div class="table-responsive invoice-table" id="table">
-                                <table class="table table-sm table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th width="95">Price</th>
-                                            <th width="10">Quantity</th>
-                                            <th width="95">Total</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach($order->products as $product)
-                                        <tr>
-                                            <td>{{ $product->name }}</td>
-                                            <td>{{ $product->price }}</td>
-                                            <td>{{ $product->quantity }}</td>
-                                            <td>{{ $product->quantity * $product->price }}</td>
-                                        </tr>
-                                        @endforeach
-                                        <tr>
-                                            <th rowspan="6" colspan="2"></th>
-                                        </tr>
-                                        <tr>
-                                            <th>Subtotal</th>
-                                            <th>{{ $order->data->subtotal }}</th>
-                                        </tr>
-                                        <tr>
-                                            <th>Advanced</th>
-                                            <th>{{ $order->data->advanced ?? 0 }}</th>
-                                        </tr>
-                                        <tr>
-                                            <th>Shipping</th>
-                                            <th>{{ $order->data->shipping_cost }}</th>
-                                        </tr>
-                                        <tr>
-                                            <th>Discount</th>
-                                            <th>{{ $order->data->discount ?? 0 }}</th>
-                                        </tr>
-                                        <tr>
-                                            <th>Total</th>
-                                            <th>{{ $order->data->shipping_cost + $order->data->subtotal - ($order->data->advanced ?? 0) - ($order->data->discount ?? 0) }}</th>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- End Table-->
-                        </div>
+                        @include('admin.orders.invoice')
+                        @include('admin.orders.invoice', [
+                            'class' => 'only-print',
+                        ])
                         <!-- End InvoiceBot-->
                     </div>
                     <div class="col-sm-12 print-edit-buttons text-center mt-3">
